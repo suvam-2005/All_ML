62c50c4abd57744cadab00085c88ce66e58eca68
suvam-2005 <professional.suvamdas@gmail.com>
2025-12-29 10:15:50 +0530

    PCA

---
+++ PCA.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import PCA
+
+df = pd.read_csv("/mnt/data/1892eddb-a755-4fb2-9d26-9843de4c9b51.csv")
+
+num_cols = df.select_dtypes(include=["float64","int64"]).columns
+cat_cols = df.select_dtypes(include=["object"]).columns
+
+num_pipe = Pipeline([
+    ("imputer", SimpleImputer(strategy="mean")),
+    ("scaler", StandardScaler())
+])
+
+cat_pipe = Pipeline([
+    ("imputer", SimpleImputer(strategy="most_frequent")),
+    ("encoder", OneHotEncoder(drop="first"))
+])
+
+preprocessor = ColumnTransformer([
+    ("num", num_pipe, num_cols),
+    ("cat", cat_pipe, cat_cols)
+])
+
+X = preprocessor.fit_transform(df)
+
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X)
